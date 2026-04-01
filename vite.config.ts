2e90f1c4aa37cc479c2ecc7a34377b423c30b514
UrošKukovec <uros-kukovec@outlook.com>
2026-04-01 16:38:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["osrs-drop-simulator.onrender.com", "osrs-drop-simulator.com"],
+  },
 })
